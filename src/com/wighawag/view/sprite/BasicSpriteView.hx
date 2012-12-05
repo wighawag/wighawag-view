3ca6821f77435515da786daabadeab238de40104
Wighawag <wighawag@gmail.com>
2012-12-05 15:31:08 +0530

    work with the new spriteutils API

--- src/com/wighawag/view/sprite/BasicSpriteView.hx
+++ src/com/wighawag/view/sprite/BasicSpriteView.hx
@@ -31,19 +31,18 @@ class BasicSpriteView implements EntityView<NMEDrawingContext>{
 
     public function draw(context:NMEDrawingContext):Void {
         var sprite = sprites.get(assetComponent.assetId);
-        var drawMode : SpriteDraw = SpriteDraw.NoScale;
         if (assetComponent.scale){
-            drawMode = SpriteDraw.Scale;
+            sprite.drawScaled(context, stateComponent.state, stateComponent.elapsedTime, Std.int(placementComponent.x), Std.int(placementComponent.y), Std.int(placementComponent.width), Std.int(placementComponent.height));
+        }else if (assetComponent.fillVertically && assetComponent.fillVertically){
+            sprite.fill(context, stateComponent.state, stateComponent.elapsedTime, Std.int(placementComponent.x), Std.int(placementComponent.y), Std.int(placementComponent.width), Std.int(placementComponent.height));
+        }else if (assetComponent.fillHorizontally){
+            sprite.fillHorizontally(context, stateComponent.state, stateComponent.elapsedTime, Std.int(placementComponent.x), Std.int(placementComponent.y), Std.int(placementComponent.width));
+        }else if (assetComponent.fillVertically){
+            sprite.fillVertically(context, stateComponent.state, stateComponent.elapsedTime, Std.int(placementComponent.x), Std.int(placementComponent.y), Std.int(placementComponent.height));
         }else{
-            if (assetComponent.fillVertically && assetComponent.fillVertically){
-                drawMode = SpriteDraw.FillAll;
-            }else if (assetComponent.fillVertically){
-                drawMode = SpriteDraw.FillVertically;
-            }else if (assetComponent.fillHorizontally){
-                drawMode = SpriteDraw.FillHorizontally;
-            }
+            sprite.draw(context, stateComponent.state, stateComponent.elapsedTime, Std.int(placementComponent.x), Std.int(placementComponent.y));
         }
-        sprite.draw(context, stateComponent.state, stateComponent.elapsedTime, Std.int(placementComponent.x), Std.int(placementComponent.y), Std.int(placementComponent.width), Std.int(placementComponent.height), drawMode);
+
     }
 
 
